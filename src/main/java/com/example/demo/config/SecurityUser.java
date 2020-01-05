10492d92f325ac2ca3e9e3fcbd8b96bba01a28f4
Hlyang <Hlyang7036@gmail.com>
2020-01-05 22:34:52 +0900

    시큐리티 유저정보 JPA와 연동기능테스트

---
+++ src/main/java/com/example/demo/config/SecurityUser.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import com.example.demo.domain.Member;
+
+public class SecurityUser extends User{
+
+	private static final long serialVersionUID = 1L;
+
+	public SecurityUser (Member member) {
+		super(member.getId(), "{noop}" + member.getPassword(),
+				AuthorityUtils.createAuthorityList(member.getRole().toString()));
+		
+	}
+	
+	
+}
